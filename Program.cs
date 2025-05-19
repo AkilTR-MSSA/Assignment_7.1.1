07fbf50d68dc6b242af43751bb33ea1c7be52e96
Akil Turner-Richards <ki1orich@gmail.com>
2025-05-19 15:38:14 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿int[] scores = { 89, 75, 72, 91, 100, 99, 82, 67 };
+SelectionSort(scores);
+Console.WriteLine(string.Join(", " , scores));
+
+void SelectionSort(int[] arr)
+{
+    for(int i = 0; i < arr.Length; i++)
+    {
+        int min = i;
+        for(int j = i + 1; j < arr.Length; j++)
+        {
+            if(arr[j] < arr[min])
+            {
+                min = j;
+            }
+        }
+        int temp = arr[i];
+        arr[i] = arr[min];
+        arr[min] = temp;
+    }
+}
